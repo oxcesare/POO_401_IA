5498f103ecae95c7b2594921b058fafe906bcb3d
César <cesar180687@hotmail.com>
2026-07-06 08:58:16 -0600

    Cambios

--- src/main/java/unidad1/inferencia/InferenciaService.java
+++
@@ -1,9 +0,0 @@
-package unidad1.inferencia;
-
-import mx.edu.unit1.neural.model.RedNeuronal; // Importando desde el JAR
-
-public class InferenciaService {
-    public void ejecutar() {
-        RedNeuronal miRed = new RedNeuronal(8, new int[10], "activada"); // ¡Ya puedes usarla!
-    }
-}
